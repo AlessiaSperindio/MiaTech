ff657198b042eba256a7fa49140be438e2747ee2
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-04 10:36:50 +0100

    ifelse

---
+++ PROGETTO JAVASCRIPT/ciclo do-while/dow.js
@@ -0,0 +1,10 @@
+let numero= prompt ("numero");
+let word= "document";
+console.log(numero);
+do{parola=word()}
+while{ (word.lenght<5);
+    console.log(word);
+}
+
+
+    
---
+++ PROGETTO JAVASCRIPT/if&else/ifelse.js
@@ -0,0 +1,6 @@
+let number=8;
+if (number >5) {
+console.log("la variabile è maggiore di 5");
+} 
+else ( number <5)
+{console.log("la variabile è minore di 5");}
