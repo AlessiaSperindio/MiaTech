0cb7642cb169239958cde424cdf469457e4cd083
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-04 10:49:26 +0100

    "ieei " score text

---
+++ PROGETTO JAVASCRIPT/IF,ELSE,ELSE-IF/ieei.js
@@ -0,0 +1,9 @@
+let score=(prompt=parseInt);
+if(score [100]&[90]){
+console.log("voto ottimo")
+}else if([70]&[89])
+{console.log("voto buono")}
+else if([60]&[69])
+    {console.log("voto sufficiente")}
+else if([0]&[59])
+    {console.log("voto insufficiente")}
