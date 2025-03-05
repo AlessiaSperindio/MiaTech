3cffc538ab30fef11d6d14ed0fd03c708af00064
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-05 13:13:46 +0100

    create an object

---
+++ PROGETTO JAVASCRIPT/POPOLAMENTO ARRAY/poparr.js
@@ -0,0 +1,17 @@
+let parola=[];
+let animal=["rabbit","pig","horse","bear","bird"];
+for(let animals=0;animals<=5; animals++ )
+{
+parola = "tell me 5 names of animals:"+ animal;}
+
+console.log(parola);
+
+let odd= [1,3,5,7,9,11]
+
+    for (let length = 0; parola.length < 5 ; parola.length++) {
+      if (parola.length[odd] == 1) {
+        return odd+(parola.length) ;
+      }
+    }
+  
+console.log (parola);
