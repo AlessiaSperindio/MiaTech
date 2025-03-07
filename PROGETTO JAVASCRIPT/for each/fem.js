e28916d95416fca974f051216d49aa1b72968b00
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-07 15:55:44 +0100

    for each map

---
+++ PROGETTO JAVASCRIPT/for each/fem.js
@@ -0,0 +1,17 @@
+let numbers = [
+    1,
+    2,
+    3,
+    4,
+    5
+];
+
+let map = numbers.map((x) => x * x);
+
+console.log(map);
+
+numbers.forEach(function (value) {
+    console.log(value * 2);
+});
+
+console.log(numbers);
