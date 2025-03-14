81ee6be624dd7a6ba644b1aca8509c4b6c11346a
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-14 23:02:39 +0100

    second:aggiungiChilometraggio

--- PROGETTO JAVASCRIPT/settimana 10-16.03/auto.js
+++ PROGETTO JAVASCRIPT/settimana 10-16.03/auto.js
@@ -2,7 +2,7 @@
 crea una classe chiamata Automobile con le seguenti proprietà:
 marca, modello, anno.
 crea un costruttore per inizializzare queste proprietà.
-aggiungiun metodo descrizione() che restituisce una stringa con una descrizione dell'automobile.
+aggiungiun metodo descrizione() che restituisce una ba con una descrizione dell'automobile.
 class Rectangle {
   constructor(height, width) {
     this.height = height;
@@ -44,6 +44,44 @@ class Automobile {
     descrizione() {
         return (this.marca) + (this.modello) + (this.anno);
     }
+    
 }
 const Audi = new Automobile("Audi", "A4", 2007,);
 console.log(Audi.descrizione());
+const audikm= new Automobile ("Audi","A4",2007,"100000km")
+console.log(audikm.descrizione());
+/*aggiungi metodi e proprietà:
+aggiungi una proprietà chilometraggio alla classe Automobile.*/
+/*function Addkm (marca,modello,anno,chilometraggio) {
+    this.marca= marca
+    this.modello = modello
+    this.anno = anno
+  this.chilometraggio = chilometraggio;
+}
+  const aggiungiChilometri= new Automobile("Audi","A4",2007,"100'000 km");
+    /*
+aggiungi un metodo aggiungiChilometri(km) che aumenta il chilometraggio dell'automobile.
+Aggiungi un metodo mostraChilometraggio() che restituisce il chilometraggio attuale dell'automobile.*/
+
+
+/*function addkm (){
+    return this.chilometraggio()+$
+
+}
+let addkm= new Automobile ("100'000 km");*/
+/*console.log(this.Addkm());*/
+
+  
+Automobile.prototype.chilometraggio = "100000km"
+let km = new Automobile ("Audi","A4",2010,"100000km")
+ console.log(km.chilometraggio);
+  
+ function Aggiungikm(chilometraggio){
+    this.chilometraggio=chilometraggio
+ return "100500km";
+}
+const audiA4 = new Automobile("Audi","A4",2007,"100000km","150000km")
+console.log(audiA4,Aggiungikm(km));
+
+/*get: function () {return this.firstName + " " + this.lastName;}
+});*/
