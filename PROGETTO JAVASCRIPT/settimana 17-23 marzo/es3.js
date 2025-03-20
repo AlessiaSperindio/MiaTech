d967744a171f68334ef6958e20f1dc0133ea19fe
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-20 11:04:00 +0100

    es 3 completed

---
+++ PROGETTO JAVASCRIPT/settimana 17-23 marzo/es3.js
@@ -0,0 +1,21 @@
+const array = [1, 2, 3];
+const [ale, anna, ele] = array;
+
+const mum = ale;
+const  firstbaby = anna;
+const secondbaby = ele;
+console.log(ale);
+
+console.log(anna);
+
+console.log(ele);
+const [...other] = array;
+const duo =[4];
+const tris = [5];
+console.log(duo);
+console.log(tris);
+
+
+
+
+
