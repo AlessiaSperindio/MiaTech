81e24465009a964a1e5dd2da2cf145b91ef681be
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-20 11:38:28 +0100

    destructuring object es 4 completed

---
+++ PROGETTO JAVASCRIPT/settimana 17-23 marzo/es4.js
@@ -0,0 +1,18 @@
+const object = {
+    nome:" alessia",
+    cognome:"sperindio",
+    anni:"31",
+    hobby:"meditazione",
+    passioni:"judo",
+    figli:("Annapaola","Vittoria Elena"),
+};
+const {nome,cognome,anni,hobby,passioni,figli} = object;
+const ale = nome;
+
+console.log(nome);
+const skills = {anni,
+    passioni};
+
+
+console.log(ale);
+console.log(skills);
