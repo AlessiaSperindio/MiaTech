b7c8404befbc7e3a0e9972af0b5316352b355418
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-21 06:41:15 +0100

    es 7 completed

---
+++ PROGETTO JAVASCRIPT/settimana 17-23 marzo/es7.js
@@ -0,0 +1,17 @@
+const numbersOne = [1, 2, 3, 4, 5, 6];
+
+
+
+const[one, two ,three, four,five,six] = numbersOne;
+
+const numbersCombined = [...numbersOne, ];
+
+console.log(numbersCombined);
+const oddNumbers = [1,3,7,9];
+
+
+const odd = [...oddNumbers, ...numbersOne ];
+console.log(odd);
+
+
+  
