ebbe0f9706c86ee7c1b9b5f6be721787a50c10f5
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-21 12:07:36 +0100

    completed

--- PROGETTO JAVASCRIPT/SETTIMANA 17-23 MARZO SECONDA PARTE/es11.js
+++ PROGETTO JAVASCRIPT/SETTIMANA 17-23 MARZO SECONDA PARTE/es11.js
@@ -1,10 +1,18 @@
 
 
-const [a, b, ...rest] = [4,8,12]
+const [a, b, ...rest] = [4,8,12,16]
+
+const primo = a;
+const secondo = b;
+const terzo = [rest];
 console.log(a);
 console.log(b);
 
 console.log(rest);
+
+console.log(primo);
+console.log(secondo);
+console.log(terzo);
 
 
 
