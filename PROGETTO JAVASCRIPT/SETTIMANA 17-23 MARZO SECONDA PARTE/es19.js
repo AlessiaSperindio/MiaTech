29e838f11f5261701802f6392e4916fb9b2ed1c7
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-21 16:08:56 +0100

    console.log()

---
+++ PROGETTO JAVASCRIPT/SETTIMANA 17-23 MARZO SECONDA PARTE/es19.js
@@ -0,0 +1,15 @@
+const number = 3;
+const string = "jump!";
+
+const array = [2, 4, 6, 8];
+const object = {
+    italiano: "ciao",
+    francese: "salut",
+    spagnolo: "ihola!",
+    inglese: "hello"
+}
+
+console.log(number);
+console.log(string);
+console.log(array);
+console.log(object);
