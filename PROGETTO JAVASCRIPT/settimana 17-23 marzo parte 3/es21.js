a05a2e803346647cf11a18d7e5f3d3e611fe5b43
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-21 16:03:05 +0100

    console.log,console.group,console.table

---
+++ PROGETTO JAVASCRIPT/settimana 17-23 marzo parte 3/es21.js
@@ -0,0 +1,5 @@
+console.table({'user':"Alessia", 'city':'LasVegas'}); 
+console.log("primo desiderio: vacanza di un mese in America, ",
+    "secondo desiderio: pubblicare un libro");
+
+console.group("desideri");
