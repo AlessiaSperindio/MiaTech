5705b8707f316b075eaa8c455a6927b8c4b00e8c
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-25 15:18:30 +0100

    es6

--- PROGETTO JAVASCRIPT/settimana 24-30marzo/parte2/es6.js
+++ PROGETTO JAVASCRIPT/settimana 24-30marzo/parte2/es6.js
@@ -1,25 +1,21 @@
-async function nameesempio() {
-    
-} promessa() {
-    try {
-        return await new Promise((resolve, reject) => {
-            const successo = Math.random() > 0.5;
-            setTimeout(() => {
-                if (successo) {
-                    resolve(`Operazione completata con successo!`);
-                } else {
-                    reject(`Si è verificato un errore.`);
-                }
-            }, 
-            1000
-        );
-        }
-    )
-    } finally {
-        console.log(`Questa azione viene eseguita sempre, sia in caso di successo che di errore.`);
-    }
+ async function esempioPromessa() {
+    return new Promise((resolve, reject) => {
+        
+        const successo = Math.random() > 0.5; 
+
+        setTimeout(() => {
+            if (successo) {
+                resolve("Operazione completata con successo!");
+            } else {
+                reject("Si è verificato un errore.");
+            }
+        }, 1000);
+    })
+    .finally(() => {
+        console.log("Operazione completata, indipendentemente dal risultato.");
+    });
 }
 
-
-console.log(result);
-console.log(error);
+esempioPromessa()
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
