8a1f14d4d73e1fe04f29ec600b11b6ecb0c99543
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-26 11:18:11 +0100

    es24

---
+++ PROGETTO JAVASCRIPT/settimana 24-30marzo/parte3/es23.js
@@ -0,0 +1,17 @@
+const apiUrl = 'https://api.example.com/data'; 
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; 
+async function fetchData() {
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        console.log(data); 
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+
+fetchData();
