48bb9ee0e3ef7fb89ff543c379e884a9ecb0fb88
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-03-27 10:20:42 +0100

    es22

--- PROGETTO JAVASCRIPT/settimana 24-30marzo/parte3/es22.js
+++ PROGETTO JAVASCRIPT/settimana 24-30marzo/parte3/es22.js
@@ -1,4 +1,4 @@
-fetch('https://api.example.com/data') 
+fetch('https://jsonplaceholder.typicode.com/comments') 
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
