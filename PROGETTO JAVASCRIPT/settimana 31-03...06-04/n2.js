3ee3e0e7c9166cd7a38f0de5a8c52321df9742c4
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-04-02 10:16:56 +0200

    n2

--- PROGETTO JAVASCRIPT/settimana 31-03...06-04/n2.js
+++ PROGETTO JAVASCRIPT/settimana 31-03...06-04/n2.js
@@ -3,16 +3,16 @@ const $valore = document.getElementById("cognome");
 
 function salvaNome(chiave, valore) {
     localStorage.setItem(chiave, valore);
-    console.log("Nome salvato:$chiave = $valore");
+    console.log(` Nome salvato:${chiave} = ${valore}` );
 }
 
 
 function recuperaNome(chiave) {
     const Nome = localStorage.getItem(chiave);
     if (Nome) {
-        console.log("Nome recuperato:$chiave = $valore");
+        console.log(` Nome recuperato:${chiave} ` );
     } else {
-        console.log("Nessun Nome trovato per la chiave: $chiave");
+        console.log(` Nessun Nome trovato per la chiave: ${chiave}` );
     }
     return $valore;
 }
@@ -20,12 +20,12 @@ function recuperaNome(chiave) {
 
 function rimuoviNome(chiave) {
     localStorage.removeItem(chiave);
-    console.log("Nome rimosso per la chiave: $chiave");
+    console.log(` Nome rimosso per la chiave: ${chiave}` );
 }
 
 
 salvaNome('nome', 'cognome');
 recuperaNome('nome');
 rimuoviNome('nome');
-recuperaNome('nome');
+
 
