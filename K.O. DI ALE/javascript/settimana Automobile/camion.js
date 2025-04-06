61d52013c827f560e9072e7b5bef037a3b9401a8
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-04-06 18:37:49 +0200

    camion

--- K.O. DI ALE/javascript/settimana Automobile/camion.js
+++ K.O. DI ALE/javascript/settimana Automobile/camion.js
@@ -10,13 +10,17 @@ class Automobile {
     }
 };
 
+
 class Camion extends Automobile {
-    constructor(discoOrario=0) {
-        super(discoOrario)
-            this.discoOrario = discoOrario;
-        }
-    };
+    constructor(marca, modello, chilometraggio, capacitàCarico) {
+        super(marca, modello, chilometraggio);
+        this.capacitàCarico = capacitàCarico;
+    }
+};
 
 
-const foodTruck = new Camion (5);
-console.log(foodTruck);
+const miaAuto = new Automobile('saab', 'model3', 10000);
+
+
+const mioCamion = new Camion('Iveco', 'Stralis', 50000, 12000);
+console.log(mioCamion);
