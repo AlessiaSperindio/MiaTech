a35fac28d19f885a91adf300403f4583ba8b6e19
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-04-06 18:27:50 +0200

    setter

--- K.O. DI ALE/javascript/settimana Automobile/setter.js
+++ K.O. DI ALE/javascript/settimana Automobile/setter.js
@@ -9,28 +9,35 @@ class Automobile {
         this._chilometraggio = chilometraggio;
     }
 
-    getChilometraggio() {
+    
+    get chilometraggio() {
         return this._chilometraggio;
     }
-    setChilometraggio(Chilometraggio2 = 0) {
-        if (Chilometraggio2 >= this._chilometraggio) {
-            this._chilometraggio = Chilometraggio2;
+
+    
+    set chilometraggio(nuovoChilometraggio) {
+        if (nuovoChilometraggio >= this._chilometraggio) {
+            this._chilometraggio = nuovoChilometraggio;
+            console.log(`Chilometraggio aggiornato a: ${this._chilometraggio}`);
         } else {
-            console.log("Errore: il chilometraggio non può essere diminuito.");
+            console.log("Errore: Il nuovo chilometraggio deve essere maggiore o uguale al chilometraggio attuale.");
         }
     }
-};
+}
+
+
+
 
 
 const setkm = new Automobile("fiat", "panda", 2000, 99);
-console.log(setkm.setChilometraggio());
+
 const setKmTRoc = new Automobile("volkwagen", "troc", 2020, 100000);
 setkm.Chilometraggio2 = 900000;
-console.log(setKmTRoc.setChilometraggio());
+
+console.log(`Chilometraggio attuale: ${setkm.chilometraggio}`);
 
 
 setkm.chilometraggio = 12000;
-console.log(setkm.chilometraggio);
+
 
 setKmTRoc.chilometraggio = 9000;
-console.log(setKmTRoc.chilometraggio); 
