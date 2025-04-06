b8be03e8bad1a2adac28c4f04fc6ac5ca88cc157
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-04-06 17:41:37 +0200

    restoperator

---
+++ K.O. DI ALE/javascript/rest operator/restoperator.js
@@ -0,0 +1,9 @@
+const numeri = [10, 20, 30, 40, 50];
+
+
+const [primo, secondo, ...resto] = numeri;
+
+
+console.log('Primo elemento:', primo);   
+console.log('Secondo elemento:', secondo); 
+console.log('Resto degli elementi:', resto); 
