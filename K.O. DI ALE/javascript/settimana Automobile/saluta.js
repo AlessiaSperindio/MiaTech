3765307c57f0dc5f20988ec02a269359241788f0
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-04-06 16:39:57 +0200

    saluta

---
+++ K.O. DI ALE/javascript/settimana Automobile/saluta.js
@@ -0,0 +1,51 @@
+class Automobile {
+    constructor(marca, modello, anno, chilometraggio) {
+      this.marca = marca;
+      this.modello = modello;
+      this.anno = anno;
+      this.chilometraggio = chilometraggio
+    }
+    descrizione() {
+      return (`questa ${this.marca} è del modello ${this.modello}`);
+    }
+  
+  
+    aggiungiChilometri = (km) => {
+      this.chilometraggio += km;
+    }
+  
+    mostraChilometraggio = () => {
+      return `${this.modello} ha ${this.chilometraggio} km.`
+    }
+  
+  }
+  
+  
+  class Elettrica extends Automobile {
+  
+    constructor(marca, modello, anno, chilometraggio, autonomia) {
+      super(marca, modello, anno, chilometraggio);
+      this.autonomia = autonomia;
+    }
+  
+    descrizione() {
+      return super.descrizione() +  `con un'autonomia di ${this.autonomia} km`
+    }
+    ricarica = (km) => {
+      this.autonomia += km;
+    }
+  
+  }
+  Elettrica.prototype.saluta = function () {
+    console.log(`hi! My name is ${this.marca} and ${this.modello}`);
+};
+  const myCar = new Automobile('fiat', 'panda', 2020, 10000)
+  const elettrica = new Elettrica('tesla', 'model1', 2025, 10000, 400)
+  
+  elettrica.ricarica(1000)
+  
+  console.log(elettrica.descrizione())
+  Automobile.prototype.saluta = function() {
+    return `Ciao! Sono un'auto ${this.marca} ${this.modello}.`
+};
+
