cbea9081b5be4171230acd2928bb1156bbd64c6e
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-04-27 22:13:01 +0200

    secondList

---
+++ progetti react/exercises/src/assets/components/SecondList.jsx
@@ -0,0 +1,13 @@
+import ItemList from "./ItemList";
+const SecondList = () =>{
+return (
+    <>
+    {
+        <ul>
+            <ItemList/>
+        </ul>
+    }
+    </>
+)
+}
+export default SecondList
