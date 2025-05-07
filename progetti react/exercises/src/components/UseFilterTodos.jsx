78be7625bda9f92fb1abf8c70fcfb6a3fad2f473
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-05-07 16:16:26 +0200

    modified error

--- progetti react/exercises/src/components/UseFilterTodos.jsx
+++ progetti react/exercises/src/components/UseFilterTodos.jsx
@@ -1,11 +1,50 @@
 import React from "react";
 //Crea un hook personalizzato chiamato useFilteredTodos che accetta una lista di to-do e un termine di ricerca. Utilizza useEffect per filtrare i to-do in base al termine di ricerca e restituisci i to-do filtrati.
             
-const UseFilterTodos =()=>{
+import { useFilteredTodos } from "../src/hooks/useFilteredTodos";
 
-    return(
+const url = "https://jsonplaceholder.typicode.com/comments";
+
+const UseFilterTodos = () => {
+    const { data } = useFilteredTodos(url, { method: "GET" });
+    return (
         <>
-        </>
-    )
+            <table>
+                <thead>
+                    <tr>
+                        <th>postId</th>
+                        <th>id</th>
+                        <th>name</th>
+                        <th>email</th>
+                        <th>body</th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+                    {data === null || data.length === 0 ? (
+                        <tr>
+                            <td colSpan={4}>
+                                empty data
+                            </td>
+                        </tr>
+                    ) : (
+                        data.map((item) => (
+                            <tr key={item.postId}>
+
+
+                                <td>{item.postId}</td>
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.body}</td>
+                            </tr>
+                        ))
+                    )
+                    }
+
+                </tbody>
+            </table>
+        </>)
 }
 export default UseFilterTodos;
