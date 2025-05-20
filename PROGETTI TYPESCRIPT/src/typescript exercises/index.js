4979747826e9f9019dfebb4107824c06373a05f6
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-05-20 08:42:07 +0200

    create typescript

---
+++ PROGETTI TYPESCRIPT/src/typescript exercises/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const myName = "Alessia";
+const myAge = 32;
+console.log(myName);
+console.log(myAge);
---
+++ PROGETTI TYPESCRIPT/src/typescript exercises/index.ts
@@ -0,0 +1,5 @@
+const myName: string= "Alessia";
+const myAge:number = 32;
+
+console.log(myName);
+console.log(myAge);
