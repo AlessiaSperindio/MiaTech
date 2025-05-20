3a30e7447d34a7c96f41e84b72fb09f7d52934ac
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-05-20 16:22:28 +0200

    todo

---
+++ progetto typescript2/src/typescript/types.js
@@ -0,0 +1,22 @@
+"use strict";
+//se inserisco un interfaccia:
+/*interface UserAutInfo{
+    isAuth:boolean,
+    Verify : boolean
+}*/
+//const array:(string|number|null)[];diventa poi :
+var todos = function (arr, obj) {
+    console.log(arr, obj);
+};
+/*const printArray (array,{
+    name:"ale",
+    age: 32,
+    isAuth: true,
+    birth: new Date(),
+    colors:array,
+    auth:{
+        isAuth:true,
+        Verify: false
+    }
+});*/
+console.log(todos);
