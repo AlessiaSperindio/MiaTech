13ef58cd07bcd1bc1b68c8c35be530efea60cc06
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-05-20 09:14:05 +0200

    second-exercise-of-typescript

--- PROGETTI TYPESCRIPT/src/typescript exercises/index.js
+++ PROGETTI TYPESCRIPT/src/typescript exercises/index.js
@@ -3,3 +3,18 @@ const myName = "Alessia";
 const myAge = 32;
 console.log(myName);
 console.log(myAge);
+//string, number & boolean
+// const myName: string = "Alessia";
+//const myAge:number = 32;
+const isAuth = true;
+const user = { name: string, age: number, isAuth: boolean } = {
+    name: "alessia",
+    age: 32,
+    isAuth: true
+};
+const colors = ["blue", "pink", "red"];
+const myFunction = (array, obj, { name: string, age: number, isAuth: boolean }) => {
+    console.log(array,obj);
+};
+myFunction(colors, { name: "Ale", age: 33, isAuth: false });
+myFunction(colors, user);
--- PROGETTI TYPESCRIPT/src/typescript exercises/index.ts
+++ PROGETTI TYPESCRIPT/src/typescript exercises/index.ts
@@ -1,5 +1,22 @@
-const myName: string= "Alessia";
-const myAge:number = 32;
+const myName: string = "Alessia";
+const myAge: number = 32;
 
 console.log(myName);
 console.log(myAge);
+
+//string, number & boolean
+// const myName: string = "Alessia";
+//const myAge:number = 32;
+const isAuth: boolean = true;
+const user :{ name: string, age: number, isAuth: boolean } = {
+    name: "alessia",
+    age: 32,
+    isAuth: true
+}
+const colors: string[] = ["blue", "pink", "red"];
+
+const myFunction = (array: string[], obj:{ name: string, age: number, isAuth: boolean }): void => {
+    console.log(array,obj);
+}
+myFunction(colors, { name: "Ale", age: 33, isAuth: false });
+myFunction(colors, user);
