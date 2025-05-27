70a7f91c78dddfdd4af29d14cefb369fcb050f82
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-05-27 16:01:46 +0200

    useNavigate

---
+++ progetti react/esercizi pagina 2 react/src/assets/components/UseNavigate.jsx
@@ -0,0 +1,25 @@
+//Aggiungi collegamenti di navigazione tra le pagine Home e About nel componente App.
+
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+
+import Home from '../../pages/Home';
+import About from '../../pages/About';
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
