db20215a85a9ef3c101c2b6ef3c2114c3c29296e
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-05-28 09:38:39 +0200

    LoginFormSubmit

---
+++ progetti react/esercizi pagina 2 react/src/assets/components/LoginFormSubmit.jsx
@@ -0,0 +1,40 @@
+// Aggiungi un bottone "Submit" che, quando cliccato, mostra un alert con l'username e la password.
+import { useState } from 'react';
+
+const LoginFormSubmit =() => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Username: ${username}\nPassword: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default LoginFormSubmit;
